7d53c9c3e3ed819e953e45d9bb52f04bb6e5b0bb
Bharathraj2107 <130839975+Bharathraj2107@users.noreply.github.com>
2024-11-11 20:32:26 +0530

    script.groovy

--- script.groovy
+++ script.groovy
@@ -1,11 +1,12 @@
+// script.groovy
 def buildApp() {
-  echo 'building the applictaion'
+  echo 'building the application'
 }
 def testApp() {
   echo 'testing the application'
 }
 def deployApp() {
-  echo 'deploying the applictaion'
+  echo 'deploying the application'
   echo "deploying version ${params.VERSION}"
 }
 return this
